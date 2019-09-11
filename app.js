24d71e22aba47cbd6970daab79a97b0b4ca08ffc
Junho Baik <junhobaik@gmail.com>
2019-09-11 22:01:11 +0900

    .

--- app.js
+++ app.js
@@ -52,6 +52,4 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at ${server.graphqlPath}`));
